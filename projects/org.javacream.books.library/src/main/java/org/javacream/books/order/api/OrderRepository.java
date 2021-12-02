857aaa4cbd1cf48aca67c2bd96c4aceff5d08448
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-12-02 07:17:01 +0100

    implement maven parents

--- projects/org.javacream.books.library/src/main/java/org/javacream/books/order/api/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package org.javacream.books.order.api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, Long>{
-
-}
--- projects/org.javacream.books.library/src/main/java/org/javacream/books/repository/api/BooksRepository.java
+++
@@ -1,7 +0,0 @@
-package org.javacream.books.repository.api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BooksRepository extends JpaRepository<BookEntity, String>{
-
-}
